37301c6f16e912b1ad6aeff942a3badd7e0f4206
Avin Joshy <avinjoshy21@gmail.com>
2024-04-26 18:26:07 +0530

    "Added 'http://localhost:5173' to allowedOrigins and changed response to return in userSignup controller"
    This commit includes changes to two files: allowedOrigins.js and authController.js.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  "http://localhost:4500"
+  "http://localhost:4500",
+  "http://localhost:5173"
 ];
 
 module.exports = allowedOrigins
--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ async function userSignup(req, res) {
     req.body.password = hashedPassword;
     const newUser = new User(req.body);
     await newUser.save();
-    res.status(201).json({ message: "User created successfully!" });
+    return res.status(201).json({ message: "User created successfully!" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "An Erorr Occured during Signup" });
