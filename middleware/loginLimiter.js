7ef757e5c2c4698492d98415f3e9dca9bbc994ba
Avin Joshy <avinjoshy21@gmail.com>
2024-05-04 10:39:32 +0530

    "Middleware: replace logEvents with logger.logEvents; update import statement"

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -1,6 +1,5 @@
 const ratelimit = require("express-rate-limit");
-const { logEvents } = require("./logger");
-
+const logger = require("./logger");
 const loginLimitter = ratelimit({
   windowMs: 60 * 1000, // 1 minutes
   limit: 5, // limit each IP to 5 login requests per window per minute.
@@ -10,7 +9,7 @@ const loginLimitter = ratelimit({
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false,
   handler: (req, res, next, options) => {
-    logEvents(
+    logger.logEvents(
       `Too many requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
       "errLog.log"
     );
